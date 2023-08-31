0b2ef4996dc2d5948481cfafb7288a89b3ae7d62
Kateryna Shymoliuk <shimolyukk@gmail.com>
2023-08-31 11:54:25 -0500

    Version one

---
+++ Calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        // Create a Scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask the user for the first number
+        System.out.print("Enter the first number: ");
+        int num1 = scanner.nextInt(); // Read the user's input
+
+        // Ask the user for the second number
+        System.out.print("Enter the second number: ");
+        int num2 = scanner.nextInt(); // Read the user's input
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Display the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+
+        // Close the scanner to free up resources
+        scanner.close();
+    }
+
+}
